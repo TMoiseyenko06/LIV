08e508324f86a9423550b264148781a259b25c65
tmoiseyenko06 <tmoiseyenko06@gmail.com>
2025-04-06 16:28:12 -0400

    added routes and controlers

--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+import userRoutes from './routes/users.js'
+import scrapbookRoutes from './routes/scrapbooks.js'
+import postRoutes from './routes/posts.js'
+
 dotenv.config()
 
 const app = express()
+
+app.use('/users', userRoutes)
+app.use('/scrapbooks', scrapbookRoutes)
+app.use('/posts',postRoutes)
 
 app.use(bodyParser.json({limit:"30mb",extended: true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended: true}))
@@ -16,5 +24,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = 27017
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{console.log('connected')})
-    .catch(()=>{})
+    .then(()=> app.listen(PORT, () => console.log(`server running: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
